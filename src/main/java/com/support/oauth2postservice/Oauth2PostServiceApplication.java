f97c84024cffb7f64e47df247aebce516489cc7c
Ryu-JeongMoon <rjm9303@gmail.com>
2022-02-07 10:43:56 +0900

    Initial commit

---
+++ src/main/java/com/support/oauth2postservice/Oauth2PostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.support.oauth2postservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2PostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2PostServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/support/oauth2postservice/Oauth2PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.support.oauth2postservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
