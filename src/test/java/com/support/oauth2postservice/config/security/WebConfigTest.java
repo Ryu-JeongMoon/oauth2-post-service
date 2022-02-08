9c2efb0edcdbb4e9364186f08fef99520ed761c0
Ryu-JeongMoon <rjm9303@gmail.com>
2022-02-09 08:55:09 +0900

    feat($security-config): add xss filter
    add lucy-xss-servlet-filter to prevent xss
    - it filters form-data value
    - need to be added xss filter or escape for request body value

---
+++ src/test/java/com/support/oauth2postservice/config/security/WebConfigTest.java
@@ -0,0 +1,27 @@
+package com.support.oauth2postservice.config.security;
+
+import com.nhncorp.lucy.security.xss.XssSaxFilter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WebConfigTest {
+
+    @Test
+    @DisplayName("Lucy-XSS-Servlet-Filter Test")
+    void xssEscape() {
+        String dirty = "<script>alert('dirty');</script>";
+
+        String clean = XssSaxFilter.getInstance().doFilter(dirty);
+        System.out.println(clean);
+        clean = clean.replace("<!-- Not Allowed Tag Filtered -->", "");
+        System.out.println(clean);
+
+        assertThat(clean).isNotEqualTo(dirty);
+        assertThat(clean.contains("<")).isEqualTo(false);
+        assertThat(clean.contains(">")).isEqualTo(false);
+        assertThat(clean.contains("&lt;")).isEqualTo(true);
+        assertThat(clean.contains("&gt;")).isEqualTo(true);
+    }
+}
