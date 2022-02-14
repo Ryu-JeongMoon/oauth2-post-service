bb04975373e5c686031951c44c47017383256af7
ryumodrn <76534087+Ryu-JeongMoon@users.noreply.github.com>
2022-02-14 17:04:57 +0900

    refactor($member-entity): add key field in role enum (#23)
    - add authority code prefix for spring-security
    - add test for role key field

---
+++ src/test/java/com/support/oauth2postservice/domain/enumeration/RoleTest.java
@@ -0,0 +1,17 @@
+package com.support.oauth2postservice.domain.enumeration;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RoleTest {
+
+    @Test
+    @DisplayName("key 출력 확인")
+    void getKey() {
+        assertThat(Role.USER.getKey()).isEqualTo("ROLE_USER");
+        assertThat(Role.ADMIN.getKey()).isEqualTo("ROLE_ADMIN");
+        assertThat(Role.MANAGER.getKey()).isEqualTo("ROLE_MANAGER");
+    }
+}
