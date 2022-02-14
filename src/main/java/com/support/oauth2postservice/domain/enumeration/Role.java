5ef9049db576f478b6a589807bf7ccc5ee99dccc
Ryu-JeongMoon <rjm9303@gmail.com>
2022-02-14 16:46:54 +0900

    refactor($member-entity): add key field in role enum
    - add authority code prefix for spring-security
    - add test for role key field

--- src/main/java/com/support/oauth2postservice/domain/enumeration/Role.java
+++ src/main/java/com/support/oauth2postservice/domain/enumeration/Role.java
@@ -1,7 +1,16 @@
 package com.support.oauth2postservice.domain.enumeration;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum Role {
-    USER,
-    MANAGER,
-    ADMIN
+
+    USER("ROLE_USER"),
+    MANAGER("ROLE_MANAGER"),
+    ADMIN("ROLE_ADMIN");
+
+    private final String key;
 }
