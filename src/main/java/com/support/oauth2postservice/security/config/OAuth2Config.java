1c86a9d3f26508d59206e305c8bfb9aa2e9ea839
ryumodrn <76534087+Ryu-JeongMoon@users.noreply.github.com>
2022-02-16 21:19:39 +0900

    refactor($security-config): refactor dynamic binding *.yml properties (#27)
    - add enum instead of string-list for type safety
    - add configuration-properties annotation for getting rid of unnecessary static string field
    - add test code for oauth2-config

--- src/main/java/com/support/oauth2postservice/security/config/OAuth2Config.java
+++ src/main/java/com/support/oauth2postservice/security/config/OAuth2Config.java
@@ -2,43 +2,42 @@ package com.support.oauth2postservice.security.config;
 
 import com.support.oauth2postservice.security.oauth2.CustomOAuth2Provider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableConfigurationProperties(value = OAuth2Properties.class)
 public class OAuth2Config {
 
-    private final static Collection<String> CLIENTS = Arrays.asList("google", "naver");
-    private final static String CLIENT_PROPERTY_PREFIX = "spring.security.oauth2.client.registration.";
-    private final Environment environment;
+    private final OAuth2Properties oAuth2Properties;
 
     @Bean
     public ClientRegistrationRepository clientRegistrationRepository() {
-        List<ClientRegistration> registrations = CLIENTS.stream()
+        List<ClientRegistration> registrations = oAuth2Properties.getRegistration()
+                .keySet()
+                .stream()
                 .map(this::getRegistration)
                 .collect(Collectors.toList());
 
         return new InMemoryClientRegistrationRepository(registrations);
     }
 
-    private ClientRegistration getRegistration(String client) {
-        String clientId = environment.getProperty(CLIENT_PROPERTY_PREFIX + client + ".client-id");
-        String clientSecret = environment.getProperty(CLIENT_PROPERTY_PREFIX + client + ".client-secret");
+    private ClientRegistration getRegistration(OAuth2Properties.Client client) {
+        OAuth2Properties.Resource resource = oAuth2Properties.getRegistration().get(client);
+        String clientName = resource.getClientName();
 
-        return CustomOAuth2Provider.valueOf(client.toUpperCase())
-                .getBuilder(client)
-                .clientId(clientId)
-                .clientSecret(clientSecret)
+        return CustomOAuth2Provider.valueOf(clientName.toUpperCase())
+                .getBuilder(clientName)
+                .clientId(resource.getClientId())
+                .clientSecret(resource.getClientSecret())
                 .build();
     }
 }
