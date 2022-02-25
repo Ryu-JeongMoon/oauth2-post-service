ff91f25547e934a9b4eb407343c963bf3173bfb1
Ryu-JeongMoon <rjm9303@gmail.com>
2022-02-25 12:49:48 +0900

    refactor($exception): refactor formation of exception-handler
    - separate handlers by response-status-code

--- src/main/java/com/support/oauth2postservice/controller/TestController.java
+++ src/main/java/com/support/oauth2postservice/controller/TestController.java
@@ -6,11 +6,16 @@ import com.support.oauth2postservice.service.post.dto.response.PostReadResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.ConstraintViolationException;
 
 @Slf4j
 @Controller
@@ -36,7 +41,7 @@ public class TestController {
     return "home";
   }
 
-  @GetMapping("/error-please")
+  @GetMapping("/error-please1")
   public void errorBomb() {
     throw new IllegalArgumentException("YAHOO ~~~~~");
   }
@@ -44,6 +49,26 @@ public class TestController {
   @GetMapping("/error-please2")
   public void errorBomb2() {
     throw new IllegalStateException("HOORAY ~~~~~");
+  }
+
+  @GetMapping("/error-please3")
+  public void errorBomb3() {
+    throw new ConstraintViolationException("HOORAY ~~~~~", null);
+  }
+
+  @GetMapping("/error-please4")
+  public void errorBomb4() {
+    throw new BadCredentialsException("HOORAY ~~~~~");
+  }
+
+  @GetMapping("/error-please5")
+  public void errorBomb5() {
+    throw new AccessDeniedException("HOORAY ~~~~~");
+  }
+
+  @GetMapping("/error-please6")
+  public void errorBomb6() throws Exception {
+    throw new Exception("HOORAY ~~~~~");
   }
 
   @GetMapping("/post")
