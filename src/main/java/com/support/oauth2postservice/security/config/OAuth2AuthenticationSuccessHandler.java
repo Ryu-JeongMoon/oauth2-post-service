9a4c6ee60581ba8cc047f8b71baef378322c6b64
Ryu-JeongMoon <rjm9303@gmail.com>
2022-03-08 19:38:39 +0900

    refactor($token-filter): change filtering-logic

--- src/main/java/com/support/oauth2postservice/security/config/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/support/oauth2postservice/security/config/OAuth2AuthenticationSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.support.oauth2postservice.security.config;
 
+import com.support.oauth2postservice.domain.enumeration.Role;
 import com.support.oauth2postservice.security.dto.OAuth2UserPrincipal;
 import com.support.oauth2postservice.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository;
 import com.support.oauth2postservice.security.oauth2.OAuth2TokenResponse;
@@ -9,12 +10,15 @@ import com.support.oauth2postservice.util.constant.UriConstants;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -24,7 +28,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
   private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
 
   @Override
-  public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+  public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
     httpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
 
     SecurityUtils.setAuthentication(authentication);
@@ -37,6 +41,15 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         .build();
 
     CookieUtils.setOAuth2TokenToBrowser(response, oAuth2TokenResponse);
-    response.sendRedirect(UriConstants.Mapping.ROOT);
+
+    String authority = principal.getAuthorities()
+        .stream()
+        .map(GrantedAuthority::getAuthority)
+        .collect(Collectors.joining(""));
+
+    if (Role.USER.getKey().equalsIgnoreCase(authority))
+      response.sendRedirect(UriConstants.Mapping.ROOT);
+    else
+      request.getRequestDispatcher(UriConstants.Mapping.MEMBERS).forward(request, response);
   }
 }
