3e95fab4408e4b65f3cb0e9cdd59446a5c92270b
ryumodrn <76534087+Ryu-JeongMoon@users.noreply.github.com>
2022-03-08 10:48:13 +0900

    feat($redis-config): redis & cache-config (#71)
    * refactor($redis-config): add test-profile
    * refactor($redis-config): restore failed test
    
    - add redis-properties to use properties in *.yml
    - add embedded-redis-config for test environment

---
+++ src/main/java/com/support/oauth2postservice/config/RedisProperties.java
@@ -0,0 +1,16 @@
+package com.support.oauth2postservice.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@ConstructorBinding
+@RequiredArgsConstructor
+@ConfigurationProperties("spring.redis")
+public class RedisProperties {
+
+  private final int port;
+  private final String host;
+}
