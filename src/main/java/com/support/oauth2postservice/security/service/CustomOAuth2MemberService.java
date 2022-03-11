3520cb39288cb672b9e1ef6d57e73af44855b496
Ryu-JeongMoon <rjm9303@gmail.com>
2022-03-11 15:01:12 +0900

    refactor($oauth2-service): refactor oauth2-login process
    - make block feature, anyone who left can not login by oauth2-service

--- src/main/java/com/support/oauth2postservice/security/service/CustomOAuth2MemberService.java
+++ src/main/java/com/support/oauth2postservice/security/service/CustomOAuth2MemberService.java
@@ -1,11 +1,14 @@
 package com.support.oauth2postservice.security.service;
 
-import com.support.oauth2postservice.domain.enumeration.AuthProvider;
 import com.support.oauth2postservice.domain.entity.Member;
+import com.support.oauth2postservice.domain.enumeration.AuthProvider;
+import com.support.oauth2postservice.domain.enumeration.Status;
 import com.support.oauth2postservice.domain.repository.MemberRepository;
 import com.support.oauth2postservice.security.oauth2.OAuth2Attributes;
+import com.support.oauth2postservice.util.exception.ExceptionMessages;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityListeners;
@@ -19,12 +22,19 @@ public class CustomOAuth2MemberService implements OAuth2MemberService {
   private final MemberRepository memberRepository;
 
   public Member getMember(String registrationId, OAuth2Attributes attributes) {
-    Optional<Member> probableMember = memberRepository.findActiveByEmail(attributes.getEmail());
-    probableMember.ifPresent(member ->
-        member.changeLatestAuthProvider(AuthProvider.valueOfCaseInsensitively(registrationId))
+    Optional<Member> probableMember = memberRepository.findByEmail(attributes.getEmail());
+    probableMember.ifPresent(member -> {
+          throwIfInactive(member);
+          member.changeLatestAuthProvider(AuthProvider.valueOfCaseInsensitively(registrationId));
+        }
     );
 
     return probableMember.orElseGet(() -> getNewlyRegistered(registrationId, attributes));
+  }
+
+  private void throwIfInactive(Member member) {
+    if (Status.INACTIVE.equals(member.getStatus()))
+      throw new AccessDeniedException(ExceptionMessages.Member.ALREADY_LEFT);
   }
 
   private Member getNewlyRegistered(String registrationId, OAuth2Attributes oAuth2Attributes) {
