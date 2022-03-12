f7163a7de64391f3c579f98e388c50878afe194b
Ryu-JeongMoon <rjm9303@gmail.com>
2022-03-13 04:05:57 +0900

    refactor($view): add new pop-up box for member-edit

--- src/main/resources/static/js/member-api.js
+++ src/main/resources/static/js/member-api.js
@@ -1,4 +1,13 @@
 function requestEdit() {
+  if ($('#password').val().length > 0 && $('#password').val().length < 4) {
+    Swal.fire({
+      icon: 'warning',
+      title: '수정할 수 없습니다',
+      text: '비밀번호는 4자 이상 입력하거나\n변경하지 않으려면 입력을 비워야 합니다',
+    });
+    return;
+  }
+
   const api_with_id = '/members/edit-page';
   const data = {
     id: $('#id').val(),
@@ -7,7 +16,6 @@ function requestEdit() {
     role: $('#role').val(),
     status: $('#status').val(),
   };
-
   const patch_data = JSON.stringify(data);
 
   $.patch(
