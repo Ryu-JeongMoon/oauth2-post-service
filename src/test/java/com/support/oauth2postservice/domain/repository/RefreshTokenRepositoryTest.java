6e64eb56c1628a14f76df1d277ebbf137969006c
Ryu-JeongMoon <rjm9303@gmail.com>
2022-03-15 13:04:21 +0900

    test($refresh-token): add test code

---
+++ src/test/java/com/support/oauth2postservice/domain/repository/RefreshTokenRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.support.oauth2postservice.domain.repository;
+
+import com.support.oauth2postservice.config.JpaTest;
+import com.support.oauth2postservice.domain.entity.Member;
+import com.support.oauth2postservice.domain.entity.RefreshToken;
+import com.support.oauth2postservice.domain.enumeration.AuthProvider;
+import com.support.oauth2postservice.helper.MemberTestHelper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RefreshTokenRepositoryTest extends JpaTest {
+
+  @Autowired
+  RefreshTokenRepository refreshTokenRepository;
+
+  RefreshToken refreshToken;
+  Member member;
+
+  @BeforeEach
+  void setUp() {
+    member = MemberTestHelper.createUser();
+    memberRepository.save(member);
+
+    refreshToken = RefreshToken.builder()
+        .member(member)
+        .expiredAt(LocalDateTime.MAX)
+        .authProvider(AuthProvider.GOOGLE)
+        .tokenValue("YAHOO")
+        .build();
+    refreshTokenRepository.save(refreshToken);
+  }
+
+  @Test
+  @DisplayName("이메일로 조회")
+  void findByEmail() {
+    boolean present = refreshTokenRepository.findByEmail(member.getEmail()).isPresent();
+
+    assertTrue(present);
+  }
+
+  @Test
+  @DisplayName("회원으로 조회")
+  void findByMember() {
+    boolean present = refreshTokenRepository.findByMember(member).isPresent();
+
+    assertTrue(present);
+  }
+
+  @Test
+  @DisplayName("토큰 값으로 조회")
+  void findByTokenValue() {
+    boolean present = refreshTokenRepository.findByTokenValue(refreshToken.getTokenValue()).isPresent();
+
+    assertTrue(present);
+  }
+}
---
+++ src/test/java/com/support/oauth2postservice/security/service/CustomOAuth2MemberServiceTest.java
@@ -0,0 +1,70 @@
+package com.support.oauth2postservice.security.service;
+
+import com.support.oauth2postservice.domain.entity.Member;
+import com.support.oauth2postservice.domain.enumeration.Status;
+import com.support.oauth2postservice.domain.repository.MemberRepository;
+import com.support.oauth2postservice.helper.MemberTestHelper;
+import com.support.oauth2postservice.security.oauth2.OAuth2Attributes;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.access.AccessDeniedException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith({MockitoExtension.class})
+class CustomOAuth2MemberServiceTest {
+
+  @Mock
+  MemberRepository memberRepository;
+  @InjectMocks
+  CustomOAuth2MemberService customOAuth2MemberService;
+
+  private final Member member = MemberTestHelper.createUser();
+  private final Member inactiveMember = MemberTestHelper.createUser();
+
+  @BeforeEach
+  void setUp() {
+    inactiveMember.changeBy(null, null, Status.INACTIVE);
+  }
+
+  @Test
+  @DisplayName("회원 조회 성공")
+  void getMember() {
+    when(memberRepository.findByEmail(member.getEmail())).thenReturn(Optional.of(member));
+
+    Map<String, Object> attributes = new HashMap<>();
+    attributes.put("email", member.getEmail());
+    attributes.put("name", member.getNickname());
+    OAuth2Attributes oAuth2Attributes = OAuth2Attributes.of("google", attributes);
+    Member result = customOAuth2MemberService.getMember("google", oAuth2Attributes);
+
+    Assertions.assertThat(result.getEmail()).isEqualTo(member.getEmail());
+
+    verify(memberRepository, times(1)).findByEmail(any());
+  }
+
+  @Test
+  @DisplayName("회원 조회 실패 - 비활성 상태")
+  void getMember_failByStatus() {
+    when(memberRepository.findByEmail(inactiveMember.getEmail())).thenReturn(Optional.of(inactiveMember));
+
+    Map<String, Object> attributes = new HashMap<>();
+    attributes.put("email", inactiveMember.getEmail());
+    attributes.put("name", inactiveMember.getNickname());
+    OAuth2Attributes oAuth2Attributes = OAuth2Attributes.of("google", attributes);
+
+    assertThrows(AccessDeniedException.class,
+        () -> customOAuth2MemberService.getMember("google", oAuth2Attributes));
+  }
+}
