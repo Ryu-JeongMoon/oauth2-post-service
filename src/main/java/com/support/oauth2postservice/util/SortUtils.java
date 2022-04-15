0a853e7795b2aacbc829e85e1bcb67863a13bce5
Ryu-JeongMoon <rjm9303@gmail.com>
2022-04-16 03:08:52 +0900

    refactor($sort-utils): rename method and add docs

--- src/main/java/com/support/oauth2postservice/util/SortUtils.java
+++ src/main/java/com/support/oauth2postservice/util/SortUtils.java
@@ -15,26 +15,35 @@ import java.util.stream.IntStream;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SortUtils {
 
+  /**
+   * sorts란 정렬 대상 컬럼을 의미하고 orders란 정렬 방향을 의미한다<br/>
+   * 리스트는 정렬 대상 컬럼을 기준으로 한다<br/>
+   * 즉 sorts.length를 이용해 <정렬 대상 컬럼, 정렬 방향> 쌍을 리스트로 만들어 반환 한다
+   */
   public static List<Pair<String, Sort.Direction>> getPairs(String[] sorts, String[] orders) {
-    String[] finalOrders = getOrAppendedIfShorterThanSorts(sorts, orders);
+    String[] computedOrders = getOrAppendedOrders(sorts, orders);
+
     return IntStream.range(0, sorts.length)
         .mapToObj(i -> {
-          String order = finalOrders[i];
-          Sort.Direction direction = Sort.Direction.DESC;
-          if (StringUtils.isNotBlank(order) && StringUtils.equalsIgnoreCase(order, Sort.Direction.ASC.name()))
-            direction = Sort.Direction.ASC;
+          String eachOrder = computedOrders[i];
+          Sort.Direction direction = StringUtils.equalsIgnoreCase(eachOrder, Sort.Direction.ASC.name())
+              ? Sort.Direction.ASC
+              : Sort.Direction.DESC;
+
           return Pair.of(sorts[i], direction);
         })
         .collect(Collectors.toList());
   }
 
-  private static String[] getOrAppendedIfShorterThanSorts(String[] sorts, String[] orders) {
-    if (ArrayUtils.getLength(sorts) > ArrayUtils.getLength(orders)) {
-      int differenceOfLength = ArrayUtils.getLength(sorts) - ArrayUtils.getLength(orders);
-
-      for (int i = 0; i < differenceOfLength; i++) {
+  /**
+   * 정렬 대상과 정렬 방향의 쌍을 맞추기 위해 입력 값의 길이가 다른 경우 강제적으로 빈 값으로 맞춰준다<br/>
+   * 정렬 컬럼 기준으로 리스트를 생성하기 때문에 정렬 방향의 배열이 더 긴 경우는 고려하지 않는다
+   */
+  private static String[] getOrAppendedOrders(String[] sorts, String[] orders) {
+    int differenceOfLength = ArrayUtils.getLength(sorts) - ArrayUtils.getLength(orders);
+    if (differenceOfLength > 0) {
+      for (int i = 0; i < differenceOfLength; i++)
         orders = Arrays.append(orders, "");
-      }
     }
 
     return orders;
