09e025832384f2e20c188af13d351ed1fcae3330
Ryu-JeongMoon <rjm9303@gmail.com>
2022-04-15 14:42:46 +0900

    feat($token-factory): add creating-refresh-token method in token-factory
    - add elliptic-curve-properties for ec-key

--- src/main/java/com/support/oauth2postservice/security/jwt/EllipticCurveFactory.java
+++ src/main/java/com/support/oauth2postservice/security/jwt/EllipticCurveFactory.java
@@ -8,6 +8,7 @@ import com.nimbusds.jose.crypto.Ed25519Verifier;
 import com.nimbusds.jose.jwk.Curve;
 import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.OctetKeyPair;
+import com.nimbusds.jose.jwk.gen.ECKeyGenerator;
 import com.nimbusds.jose.jwk.gen.OctetKeyPairGenerator;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
@@ -17,12 +18,10 @@ import com.support.oauth2postservice.util.constant.Times;
 import com.support.oauth2postservice.util.constant.TokenConstants;
 import com.support.oauth2postservice.util.exception.ExceptionMessages;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
-
-import java.util.Date;
-import java.util.UUID;
 
 /**
  * The 2021 TLS Telemetry Report 보고서에 따르면 많은 웹사이트가 점점 TLS v1.3으로 옮겨가는 추세이고<br/>
@@ -35,16 +34,17 @@ import java.util.UUID;
  */
 @Slf4j
 @Component
+@EnableConfigurationProperties(EllipticCurveProperties.class)
 public class EllipticCurveFactory extends TokenFactory {
 
   private final OctetKeyPair privateJsonWebKey;
   private final OctetKeyPair publicJsonWebKey;
   private final Ed25519Signer ed25519Signer;
 
-  public EllipticCurveFactory() throws JOSEException {
+  public EllipticCurveFactory(EllipticCurveProperties ellipticCurveProperties) throws JOSEException {
     privateJsonWebKey = new OctetKeyPairGenerator(Curve.Ed25519)
         .keyUse(KeyUse.SIGNATURE)
-        .keyID(UUID.randomUUID().toString())
+        .keyID(ellipticCurveProperties.getSecretKey())
         .generate();
     ed25519Signer = new Ed25519Signer(privateJsonWebKey);
 
@@ -57,28 +57,28 @@ public class EllipticCurveFactory extends TokenFactory {
   }
 
   @Override
-  protected String createAccessToken(Authentication authentication) {
-    long currentTime = new Date().getTime();
-
-    String authorities = authentication.getAuthorities().stream()
-        .map(grantedAuthority -> grantedAuthority.getAuthority().replace(TokenConstants.ROLE_PREFIX, ""))
-        .findAny()
-        .orElseGet(Role.USER::name);
-
-    JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
-        .subject(authentication.getName())
-        .issuer(TokenConstants.LOCAL_TOKEN_ISSUER)
-        .claim(TokenConstants.AUTHORITIES, authorities)
-        .claim(TokenConstants.TOKEN_TYPE, TokenConstants.ACCESS_TOKEN)
-        .claim(TokenConstants.USER_ID, getIdFromAuthentication(authentication))
-        .expirationTime(new Date(currentTime + Times.ACCESS_TOKEN_EXPIRATION_MILLIS.getValue()))
-        .build();
-
+  protected String createIdToken(Authentication authentication) {
     JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.EdDSA)
         .keyID(privateJsonWebKey.getKeyID())
         .build();
 
+    JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+        .subject(authentication.getName())
+        .issuer(TokenConstants.LOCAL_TOKEN_ISSUER)
+        .claim(TokenConstants.AUTHORITIES, getAuthorities(authentication))
+        .claim(TokenConstants.TOKEN_TYPE, TokenConstants.ID_TOKEN)
+        .claim(TokenConstants.USER_ID, getIdFromAuthentication(authentication))
+        .expirationTime(createExpirationDate(Times.ACCESS_TOKEN_EXPIRATION_SECONDS))
+        .build();
+
     return createNewJWT(header, claimsSet);
+  }
+
+  private String getAuthorities(Authentication authentication) {
+    return authentication.getAuthorities().stream()
+        .map(grantedAuthority -> grantedAuthority.getAuthority().replace(TokenConstants.ROLE_PREFIX, ""))
+        .findAny()
+        .orElseGet(Role.USER::name);
   }
 
   private String getIdFromAuthentication(Authentication authentication) {
@@ -90,18 +90,30 @@ public class EllipticCurveFactory extends TokenFactory {
   }
 
   @Override
+  protected String createAccessToken(Authentication authentication) {
+    try {
+      String ellipticCurvePoint = new ECKeyGenerator(Curve.P_256)
+          .keyUse(KeyUse.ENCRYPTION)
+          .generate()
+          .getX()
+          .toString();
+
+      return TokenConstants.ACCESS_TOKEN_PREFIX + ellipticCurvePoint;
+    } catch (JOSEException e) {
+      throw new IllegalStateException(e);
+    }
+  }
+
+  @Override
   protected String createRefreshToken(Authentication authentication) {
-    long currentTime = new Date().getTime();
+    JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.EdDSA)
+        .build();
 
     JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
         .subject(authentication.getName())
-        .issuer(TokenConstants.LOCAL_TOKEN_ISSUER)
-        .claim(TokenConstants.TOKEN_TYPE, TokenConstants.REFRESH_TOKEN)
-        .expirationTime(new Date(currentTime + Times.REFRESH_TOKEN_EXPIRATION_MILLIS.getValue()))
-        .build();
-
-    JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.EdDSA)
-        .keyID(privateJsonWebKey.getKeyID())
+        .claim(TokenConstants.AUTHORITIES, getAuthorities(authentication))
+        .claim(TokenConstants.USER_ID, getIdFromAuthentication(authentication))
+        .expirationTime(createExpirationDate(Times.REFRESH_TOKEN_EXPIRATION_SECONDS))
         .build();
 
     return createNewJWT(header, claimsSet);
